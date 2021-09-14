715dccbdbd170be29ae9120da0238aef9d149777
Toni Itkonen <toni.itkonen@icloud.com>
2021-09-14 09:07:38 +0300

    Project Better-you-diary skeleton and storyboards created

---
+++ Better-You-Diary/Better-You-Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Better-You-Diary
+//
+//  Created by Toni Itkonen on 14.9.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
