ad671ec25d435f8969cc2e802eb696095633c3b7
Toni Itkonen <toni.itkonen@icloud.com>
2021-09-20 15:41:55 +0300

    Show rate Int in Detail VC

--- Better-You-Diary/Better-You-Diary/Controllers/DayEntriesViewController.swift
+++ Better-You-Diary/Better-You-Diary/Controllers/DayEntriesViewController.swift
@@ -1,11 +1,8 @@
 import UIKit
 import CoreData
 
-class DayEntriesViewController: UITableViewController   { // NSFetchedResultsControllerDelegate
+class DayEntriesViewController: UITableViewController {
 
-    // MARK: IB Outlets
-    //@IBOutlet strong var tableView: UITableView!
-    
     // MARK: Properties
     lazy var coreDataStack = CoreDataStack(modelName: "better-you-datamodel")
     var daysEntity = [DaysEntity]()
@@ -17,16 +14,10 @@ class DayEntriesViewController: UITableViewController   { // NSFetchedResultsCon
       formatter.timeStyle = .medium
       return formatter
     }()
-    
-    //let context = CoreDataStack.persistentContainer.viewContext // Context Layer
-    
+        
     // MARK: VIEW LIFE CYCLE
     override func viewDidLoad() {
       super.viewDidLoad()
-
-        //navigationItem.title = "List of ideas"
-      //title = "Diary 2.3"
-      //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
 
     }
     
@@ -34,7 +25,6 @@ class DayEntriesViewController: UITableViewController   { // NSFetchedResultsCon
         super.viewWillAppear(animated)
         
         setupTableView()
-        
         
         let lataaData: NSFetchRequest<DaysEntity> = DaysEntity.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: #keyPath(DaysEntity.date), ascending: false)
@@ -53,10 +43,7 @@ class DayEntriesViewController: UITableViewController   { // NSFetchedResultsCon
         tableView.register(DaysCell.self, forCellReuseIdentifier: CustomCell)
     }
 
-
-
     // MARK: TABLE VIEW DATA SOURCE
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return daysEntity.count
     }
@@ -69,15 +56,6 @@ class DayEntriesViewController: UITableViewController   { // NSFetchedResultsCon
         cell.daysData = noteForRow
         
         return cell
-        
-//        guard let showDate = daysEntity[indexPath.row] as? DaysEntity,
-//          let showDateinList = showDate.date as Date? else {
-//            return cell
-//        }
-//        cell.textLabel?.text = daysEntity[indexPath.row].text
-//        cell.detailTextLabel?.text = paivanMuotoilu.string(from: showDateinList)
-//
-//        return cell
     }
     
     ///Push content to Detail ViewController
@@ -88,10 +66,6 @@ class DayEntriesViewController: UITableViewController   { // NSFetchedResultsCon
       
       navigationController?.pushViewController(noteDetailController, animated: true)
     }
-    
-    
-    
-    
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
--- Better-You-Diary/Better-You-Diary/Controllers/DaysDetailVC.swift
+++ Better-You-Diary/Better-You-Diary/Controllers/DaysDetailVC.swift
@@ -8,15 +8,14 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     let dateFormatter: DateFormatter = {
           let dateFormatter = DateFormatter()
           dateFormatter.dateFormat = "MMMM dd, YYYY hh:mm"
-          //dateFormatter.dateFormat = "MM/dd/yy"
           return dateFormatter
     }()
     
     var daysData: DaysEntity! {
         didSet {
-          distanceLabel.text = daysData.text
-          //durationLabel.text = String(noteData.duration)
-          dateLabel.text = dateFormatter.string(from: daysData.date ?? Date())
+            distanceLabel.text = daysData.text
+            dateLabel.text = dateFormatter.string(from: daysData.date ?? Date())
+            rateLabel.text = String(daysData.rate)
         }
     }
     
@@ -26,7 +25,6 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
         label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         label.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
         label.text = dateFormatter.string(from: daysData.date ?? Date())
-        //label.text = "HELLO HELLO 3"
         label.textAlignment = .left
         return label
     }()
@@ -34,10 +32,19 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     fileprivate lazy var distanceLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 40, weight: .semibold)
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         label.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
-        //label.text = dateFormatter.string(from: Date())
-        label.text = "distance !!!"
+        label.text = "Thoughts of the day"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    fileprivate lazy var rateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+        label.text = "Rate of the day"
         label.textAlignment = .left
         return label
     }()
@@ -55,11 +62,10 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
       
-        navigationController?.navigationBar.barTintColor = UIColor.green
-      view.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        view.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
       //print("viewWillAppear is loaded 2")
       
-      distanceLabel.text = daysData.text
+      //distanceLabel.text = daysData.text
       //durationLabel.text = String(noteData.duration) + " sec"
       //dateLabel?.text = "HELLO HELLO"
       
@@ -70,8 +76,7 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     fileprivate func setupUI() {
         view.addSubview(dateLabel)
         view.addSubview(distanceLabel)
-//        view.addSubview(durationLabel)
-//        view.addSubview(mapView)
+        view.addSubview(rateLabel)
         
         dateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
@@ -82,6 +87,10 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
         distanceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         distanceLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -640).isActive = true
       
+        rateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
+        rateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        rateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        rateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -560).isActive = true
         
       
     }
