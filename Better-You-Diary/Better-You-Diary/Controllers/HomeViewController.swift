050463e5cf59f0111abbd5ab1da595160fbc261a
Toni Itkonen <toni.itkonen@icloud.com>
2021-09-21 15:39:30 +0300

    textview clear, dismiss keyboard, HomeView: Primary button, button renaming.

--- Better-You-Diary/Better-You-Diary/Controllers/HomeViewController.swift
+++ Better-You-Diary/Better-You-Diary/Controllers/HomeViewController.swift
@@ -1,10 +1,14 @@
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextViewDelegate {
 
     //MARK: OUTLETS
     @IBOutlet weak var TextView: UITextView!
     @IBOutlet weak var rateControl: UISegmentedControl!
+    @IBOutlet weak var saveTextButton: UIButton!
+    
+    //MARK: PROPERTIES
+    
     
     //MARK: VIEW LIFE CYCLE
     override func viewDidLoad() {
@@ -12,11 +16,22 @@ class HomeViewController: UIViewController {
         
         TextView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.1)
         TextView.layer.cornerRadius = 12
+        TextView.delegate = self
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+        
+        saveTextButton.backgroundColor = UIColor(red: 237/255, green: 144/255, blue: 28/255, alpha: 1)
+        saveTextButton.layer.cornerRadius = 24.0
+        saveTextButton.tintColor = .white
+        saveTextButton.frame.size = CGSize(width: 192, height: 48)
+        saveTextButton.center.x = self.view.center.x
     }
     
+    //MARK: FUNCTIONS
     func showAlertModal() {
         let alert = UIAlertController(title: "Content saved successfully", message: "", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: { action in
 //            switch action.style{
 //                case .default:
 //                print("default")
@@ -32,6 +47,14 @@ class HomeViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        TextView.text = ""
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        TextView.resignFirstResponder()
+        return true
+    }
 
     //MARK: IB ACTIONS
     @IBAction func saveTextDateRateButtonPressed(_ sender: UIButton) {
