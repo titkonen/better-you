38e13c92600a1f62c86f06476f27d087689a2563
Toni Itkonen <toni.itkonen@icloud.com>
2021-10-11 14:25:56 +0300

    Detail VC TextField changed to TextView

--- Better-You-Diary/Better-You-Diary/Controllers/DaysDetailVC.swift
+++ Better-You-Diary/Better-You-Diary/Controllers/DaysDetailVC.swift
@@ -13,7 +13,7 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     
     var daysData: DaysEntity! {
         didSet {
-            distanceLabel.text = daysData.text
+            tekstikentta.text = daysData.text
             dateLabel.text = dateFormatter.string(from: daysData.date ?? Date())
             rateLabel.text = String(daysData.rate)
         }
@@ -29,14 +29,15 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
         return label
     }()
     
-    fileprivate lazy var distanceLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
-        label.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
-        label.text = "Thoughts of the day"
-        label.textAlignment = .left
-        return label
+    fileprivate lazy var tekstikentta: UITextView = {
+        //let label = UILabel()
+        let tekstilaatikko = UITextView()
+        tekstilaatikko.translatesAutoresizingMaskIntoConstraints = false
+        tekstilaatikko.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        tekstilaatikko.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+        tekstilaatikko.text = "Thoughts of the day"
+        tekstilaatikko.textAlignment = .left
+        return tekstilaatikko
     }()
     
     fileprivate lazy var rateLabel: UILabel = {
@@ -75,22 +76,24 @@ class DaysDetailVC: UIViewController, UINavigationControllerDelegate {
     
     fileprivate func setupUI() {
         view.addSubview(dateLabel)
-        view.addSubview(distanceLabel)
+        view.addSubview(tekstikentta)
         view.addSubview(rateLabel)
         
         dateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         
-        distanceLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
-        distanceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        distanceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        distanceLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -640).isActive = true
-      
-        rateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
+        rateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
         rateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         rateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        rateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -560).isActive = true
+        rateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -640).isActive = true
+        
+        tekstikentta.topAnchor.constraint(equalTo: view.topAnchor, constant: 220).isActive = true
+        tekstikentta.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        tekstikentta.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        tekstikentta.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -560).isActive = true
+      
+
         
       
     }
